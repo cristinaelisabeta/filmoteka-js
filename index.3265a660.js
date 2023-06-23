209910498c013866b1c98080b6c70c40209533d4
cristinaelisabeta <119240018+cristinaelisabeta@users.noreply.github.com>
2023-06-23 19:24:40 +0000

    Deploying to gh-pages from @ cristinaelisabeta/filmoteka-js@acc4b94258dd9ffc7f1968ea9b8ee758a4d76597 🚀

---
+++ index.3265a660.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("checkbox");"true"===localStorage.getItem("darkMode")&&(document.body.classList.add("dark"),e.checked=!0),e.addEventListener("change",function(){document.body.classList.toggle("dark");var e=document.body.classList.contains("dark");localStorage.setItem("darkMode",e.toString())})}();
+//# sourceMappingURL=index.3265a660.js.map
